011790804b62ea3efff3b7d31c0c490982a65967
Christopher F <foxbot@protonmail.com>
2018-10-14 12:04:34 -0400

    feedchecker: finish checking procedure

--- feed.go
+++ feed.go
@@ -1,6 +1,8 @@
 package feedbot
 
 import (
+	"fmt"
+
 	"github.com/mmcdole/gofeed"
 	"github.com/pkg/errors"
 )
@@ -21,15 +23,25 @@ func NewFeedChecker(c *Controller) (*FeedChecker, error) {
 func (f *FeedChecker) Close() {
 }
 
-func (f *FeedChecker) checkOnce() error {
+// checkOnce will loop over all feeds in the database, ping the remote, and check for
+// updates.
+//
+// for each feed, we:
+// - check the remote
+// - see if any new items have been appended
+// - make a list of new items, dispatch those elsewhere to be handled
+// - update the database with the new most-recent timestamp
+func (f *FeedChecker) checkOnce() []error {
 	feeds, err := f.controller.GetFeeds()
 	if err != nil {
-		return errors.Wrap(err, "couldn't retrieve feeds")
+		return []error{errors.Wrap(err, "couldn't retrieve feeds")}
 	}
 
 	fp := gofeed.NewParser()
 
 	var errs []error
+
+feedsLoop:
 	for _, dbFeed := range feeds {
 		feed, err := fp.ParseURL(dbFeed.URI)
 
@@ -39,8 +51,46 @@ func (f *FeedChecker) checkOnce() error {
 			continue
 		}
 
-		
+		if len(feed.Items) == 0 {
+			continue
+		}
+
+		// use the timestamp of the feed's most recent entry, rather than the feed's updated time.
+		// some generators use the timestamp of compilation to mark the feed, rather than its most
+		// recent post
+
+		recent := feed.Items[0] // TODO: are RSS feeds always sorted with most-recent at the top?
+		if recent.PublishedParsed == nil {
+			err = errors.New(fmt.Sprintf("the feed at %s contained an entry with no timestamp!", dbFeed.URI))
+			errs = append(errs, err)
+			continue
+		}
+
+		minTime := dbFeed.LastUpdated.Unix()
+		if minTime >= recent.PublishedParsed.Unix() {
+			continue
+		}
+
+		var items []*gofeed.Item
+		for _, item := range feed.Items {
+			if item.PublishedParsed == nil {
+				err = errors.New(fmt.Sprintf("the feed at %s contained an entry with no timestamp!", dbFeed.URI))
+				errs = append(errs, err)
+				continue feedsLoop
+			}
+			if minTime >= item.PublishedParsed.Unix() {
+				break
+			}
+			items = append(items, item)
+		}
+
+		// TODO: send these off to Discord
+		fmt.Printf("handled %d new items for feed %s!", len(items), dbFeed.URI)
+
+		if err = f.controller.UpdateFeedTimestamp(&dbFeed, recent.PublishedParsed); err != nil {
+			errs = append(errs, err)
+		}
 	}
 
-	return nil
+	return errs
 }
